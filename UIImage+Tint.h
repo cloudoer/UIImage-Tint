34479b9df87babf4c66e223b9b2eb3bea4354618
aiwobiezou <zlong151535@126.com>
2013-10-31 11:30:02 +0800

    Create UIImage+Tint.h

---
+++ UIImage+Tint.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Tint)
+
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor;
+- (UIImage *) imageWithGradientTintColor:(UIColor *)tintColor;
+
+@end
